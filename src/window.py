c8296b625b35f25fbe7378149403771aba8276f0
Nokse22 <lorenzo.ubaldi22@gmail.com>
2023-12-09 23:08:16 +0100

    started using xml file for ui

--- src/window.py
+++ src/window.py
@@ -59,43 +59,33 @@ class Question:
                f"Type: {self.question_type}\nCorrect Answer: {self.correct_answer}\n" \
                f"Incorrect Answers: {', '.join(self.incorrect_answers)}"
 
+@Gtk.Template(resource_path='/io/github/nokse22/trivia-quiz/window.ui')
 class TriviaWindow(Adw.ApplicationWindow):
-    __gtype_name__ = 'TriviaWindow'
+    __gtype_name__ = 'TriviaQuizWindow'
 
-    label = Gtk.Template.Child()
+    difficulty_row = Gtk.Template.Child()
+    category_row = Gtk.Template.Child()
+    type_row = Gtk.Template.Child()
+    home_button = Gtk.Template.Child()
+
+    categories_string_list = Gtk.Template.Child()
+    stack = Gtk.Template.Child()
+
+    start_button = Gtk.Template.Child()
+    start_button_stack = Gtk.Template.Child()
+
+    category_label = Gtk.Template.Child()
+    difficulty_label = Gtk.Template.Child()
+    question_label = Gtk.Template.Child()
+
+    answers_stack = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.first_box = Gtk.Box(orientation=1)
-        self.headerbar = Adw.HeaderBar(css_classes=["flat"])
-
-        menu_button = Gtk.MenuButton()
-        menu_button.set_icon_name("open-menu-symbolic")
-        menu = Gio.Menu()
-        # menu.append(_("Preferences"), "app.preferences")
-        menu.append(_("Keyboard shortcuts"), "win.show-help-overlay")
-        menu.append(_("About Trivia Quiz"), "app.about")
-        menu_button.set_menu_model(menu)
-        self.headerbar.pack_end(menu_button)
-
-        self.back_button = Gtk.Button(icon_name="go-previous-symbolic")
-        self.back_button.connect("clicked", self.on_back_button_clicked)
-        self.back_button.set_sensitive(False)
-        self.headerbar.pack_start(self.back_button)
-
-        self.set_title("")
-        self.set_default_size(600, 800)
-        self.set_size_request(400, 650)
-        self.first_box.append(self.headerbar)
-        self.clamp = Adw.Clamp(margin_start=20, margin_end=20, tightening_threshold=600, maximum_size=800)
-        self.handle = Gtk.WindowHandle(margin_bottom=10, vexpand=True)
-        self.toast_overlay = Adw.ToastOverlay()
-        self.toast_overlay.set_child(self.clamp)
-        self.handle.set_child(self.toast_overlay)
-        self.first_box.append(self.handle)
-
         self.categories = [["Any category", None]]
+        self.difficulties = [["Any difficulty", None], ["Easy", "easy"], ["Medium", "medium"], ["Hard", "hard"]]
+        self.types = [["Any type", None], ["Multiple Choice", "multiple"], ["True or False", "boolean"]]
 
         self.selected_difficulty = ""
         self.selected_category = ""
@@ -103,95 +93,36 @@ class TriviaWindow(Adw.ApplicationWindow):
         self.selected_mode = ""
         self.amount = 20
 
-        self.set_content(self.first_box)
-
-        self.start_page = self.get_start_page()
-        self.set_start_page()
-
         self.questions = []
 
         self.token = self.get_open_trivia_token()
 
         self.has_responded = False
 
-    def set_no_connection_page(self):
-        self.back_button.set_sensitive(False)
-        self.no_connectio_page = Gtk.Box(orientation=1, vexpand=True)
-
-        self.no_connectio_page.append(Gtk.Label(label="Trivia Quiz", css_classes=["large-title"], valign=Gtk.Align.CENTER, vexpand=True))
-
-        status_page = Adw.StatusPage(title="No Connection", description="There is no internet connection or the Open Trivia database is not responding",
-                icon_name="network-wireless-offline-symbolic", vexpand=True)
-        self.no_connectio_page.append(status_page)
-        self.retry_button = Gtk.Button(label="Retry", css_classes=["pill", "suggested-action"],
-                halign=Gtk.Align.CENTER, valign=Gtk.Align.CENTER, vexpand=True, margin_bottom=65)
-        self.retry_button.connect("clicked", self.on_retry_button_clicked)
-        self.no_connectio_page.append(self.retry_button)
-
-        self.clamp.set_child(self.no_connectio_page)
-        # self.set_content(self.first_box)
+        self.get_categories()
 
     def on_retry_button_clicked(self, btn):
         code = self.set_start_page()
         if code == 0:
             toast = Adw.Toast(title="Still No Connection")
-            self.toast_overlay.add_toast(toast)
+            # self.toast_overlay.add_toast(toast)
 
-    def on_back_button_clicked(self, btn):
+    @Gtk.Template.Callback("on_home_button_clicked")
+    def on_home_button_clicked(self, btn):
         self.questions = []
-        self.set_start_page()
-        self.back_button.set_sensitive(False)
+        self.stack.set_visible_child_name("home_page")
+        self.home_button.set_sensitive(False)
 
     def set_start_page(self):
-        self.clamp.set_child(self.start_page)
+        # self.clamp.set_child(self.start_page)
+        pass
 
     def get_start_page(self):
         new_categories = self.get_categories()
         if new_categories == 0:
-            self.set_no_connection_page()
+            self.stack.set_visible_child_name("error_page")
             return 0
         self.categories += new_categories
-
-        start_page = Gtk.Box(orientation=1, vexpand=True)
-
-        start_page.append(Gtk.Label(label="Trivia Quiz", css_classes=["large-title"], valign=Gtk.Align.CENTER, vexpand=True))
-
-        difficuties = [["Any difficulty", None], ["Easy", "easy"], ["Medium", "medium"], ["Hard", "hard"]]
-        difficulty_row = self.new_combo_row_from_strings("Difficulty", difficuties)
-        # for index, difficulty in enumerate(difficuties):
-        #     if self.selected_difficulty == difficulty[1]:
-        #         difficulty_row.set_selected(index)
-
-        category_row = self.new_combo_row_from_strings("Category", self.categories)
-        # for index, category in enumerate(self.categories):
-        #     if self.selected_category == category[1]:
-        #         category_row.set_selected(index)
-
-        types = [["Any type", None], ["Multiple Choice", "multiple"], ["True or False", "boolean"]]
-        type_row = self.new_combo_row_from_strings("Type", types)
-        # for index, q_type in enumerate(types):
-        #     if self.selected_difficulty == q_type[1]:
-        #         type_row.set_selected(index)
-
-        modes = [["Endless", "ENDLESS"], ["Time", "TIME"], ["No error", "NO-ERROR"]]
-        gamemode_row = self.new_combo_row_from_strings("Game mode", modes)
-        # for index, mode in enumerate(modes):
-        #     if self.selected_mode == mode[1]:
-        #         gamemode_row.set_selected(index)
-
-        settings_list = Gtk.ListBox(selection_mode=0, css_classes=["navigation-sidebar"], valign=Gtk.Align.CENTER, vexpand=True)
-
-        start_page.append(settings_list)
-
-        settings_list.append(difficulty_row)
-        settings_list.append(category_row)
-        settings_list.append(type_row)
-        # settings_list.append(gamemode_row)
-
-        start_button = Gtk.Button(label="Start", css_classes=["pill", "suggested-action"],
-                halign=Gtk.Align.CENTER, valign=Gtk.Align.CENTER, vexpand=True, margin_bottom=40)
-        start_button.connect("clicked", self.on_start_button_clicked, difficulty_row, category_row, type_row, gamemode_row)
-        start_page.append(start_button)
 
         return start_page
         # self.set_content(self.first_box)
@@ -228,41 +159,31 @@ class TriviaWindow(Adw.ApplicationWindow):
         except:
             return 0
 
-        question_page = Gtk.Box(orientation=1, vexpand=True, homogeneous=True)
-        question_box = Gtk.Box(orientation=1, vexpand=True)
-        details_box = Gtk.Box()
-        if self.selected_category == None:
-            details_box.append(Gtk.Label(label=question.category.capitalize(), hexpand=True, ellipsize=3))
-        if self.selected_difficulty == None:
-            details_box.append(Gtk.Label(label=question.difficulty.capitalize(), hexpand=True))
-        question_box.append(details_box)
-        question_box.append(Gtk.Label(label=question.question_text, css_classes=["large-title"], wrap=True, margin_bottom=10, vexpand=True))
-        question_page.append(question_box)
+        self.category_label.set_label(question.category.capitalize())
+        self.difficulty_label.set_label(question.difficulty.capitalize())
 
-        answer_box = Gtk.Box(orientation=1, vexpand=True)
-        question_page.append(answer_box)
-        buttons = []
+        self.question_label.set_label(question.question_text)
 
         if question.question_type == "multiple":
-            label = Gtk.Label(label=question.correct_answer, vexpand=True, wrap=True)
-            correct_button1 = Gtk.Button(margin_top=10, margin_bottom=10)
-            correct_button1.set_child(label)
-            buttons.append(correct_button1)
-            correct_button1.connect("clicked", self.answer_selected, correct_button1)
+            self.answers_stack.set_visible_child_name("multiple_choice_answers")
+            question.correct_answer
 
-            for answer in question.incorrect_answers:
-                label = Gtk.Label(label=html.unescape(answer), vexpand=True, wrap=True)
-                button1 = Gtk.Button(margin_top=10, margin_bottom=10)
-                button1.set_child(label)
-                buttons.append(button1)
-                button1.connect("clicked", self.answer_selected, correct_button1)
+            # correct_button1.connect("clicked", self.answer_selected, correct_button1)
+
+            # for answer in question.incorrect_answers:
+            #     html.unescape(answer)
+            #     button1 = Gtk.Button(margin_top=10, margin_bottom=10)
+            #     button1.set_child(label)
+            #     buttons.append(button1)
+            #     button1.connect("clicked", self.answer_selected, correct_button1)
         else:
-            correct_button1 = Gtk.Button(label=question.correct_answer, vexpand=True, margin_top=10, margin_bottom=10)
-            buttons.append(correct_button1)
-            correct_button1.connect("clicked", self.answer_selected, correct_button1)
-            button1 = Gtk.Button(label=question.incorrect_answers[0], vexpand=True, margin_top=10, margin_bottom=10)
-            buttons.append(button1)
-            button1.connect("clicked", self.answer_selected, correct_button1)
+            self.answers_stack.set_visible_child_name("true_false_answers")
+            # correct_button1 = Gtk.Button(label=question.correct_answer, vexpand=True, margin_top=10, margin_bottom=10)
+            # buttons.append(correct_button1)
+            # correct_button1.connect("clicked", self.answer_selected, correct_button1)
+            # button1 = Gtk.Button(label=question.incorrect_answers[0], vexpand=True, margin_top=10, margin_bottom=10)
+            # buttons.append(button1)
+            # button1.connect("clicked", self.answer_selected, correct_button1)
 
         random.shuffle(buttons)
         for button in buttons:
@@ -295,7 +216,7 @@ class TriviaWindow(Adw.ApplicationWindow):
         if len(self.questions) == 0:
             code = self.get_new_trivia_questions(1, self.selected_category, self.selected_difficulty, self.selected_type, self.token)
             if code == 0:
-                self.set_no_connection_page()
+                self.stack.set_visible_child_name("error_page")
                 return
             thread = threading.Thread(target=self.get_new_trivia_questions, args=(self.amount, self.selected_category, self.selected_difficulty, self.selected_type))
             thread.start()
@@ -303,31 +224,41 @@ class TriviaWindow(Adw.ApplicationWindow):
             thread = threading.Thread(target=self.get_new_trivia_questions, args=(self.amount, self.selected_category, self.selected_difficulty, self.selected_type))
             thread.start()
         question_page = self.new_question_page()
-        self.clamp.set_child(question_page)
 
-    def on_start_button_clicked(self, btn, difficulty_row, category_row, type_row, gamemode_row):
-        self.back_button.set_sensitive(True)
-        self.selected_difficulty = difficulty_row.get_selected_item().identification
-        self.selected_category = category_row.get_selected_item().identification
-        self.selected_type = type_row.get_selected_item().identification
-        self.selected_mode = type_row.get_selected_item().identification
+    @Gtk.Template.Callback("on_start_button_clicked")
+    def on_start_button_clicked(self, btn):
+        self.home_button.set_sensitive(True)
+        self.selected_difficulty = self.get_identification(self.difficulty_row.get_selected_item(), self.difficulties)
+        self.selected_category = self.get_identification(self.category_row.get_selected_item(), self.categories)
+        self.selected_type = self.get_identification(self.type_row.get_selected_item(), self.types)
 
         thread = threading.Thread(target=self.get_new_trivia_questions, args=(self.amount, self.selected_category, self.selected_difficulty, self.selected_type))
         thread.start()
 
+        self.start_button_stack.set_visible_child_name("spinner")
+
         if len(self.questions) == 0:
             self.get_new_trivia_questions(1, self.selected_category, self.selected_difficulty, self.selected_type, self.token)
+            self.start_button_stack.set_visible_child_name("label")
 
         self.first_question()
 
+    def get_identification(self, interface_name, lookup_array):
+        for couple in lookup_array:
+            if couple[0] == interface_name:
+                return couple[1]
+        return None
+
     def first_question(self):
+        self.stack.set_visible_child_name("question_page")
         code = question_page = self.new_question_page()
         if code == 0:
             toast = Adw.Toast(title="There is no connection")
-            self.toast_overlay.add_toast(toast)
-            self.set_no_connection_page()
+            # self.toast_overlay.add_toast(toast)
+            self.stack.set_visible_child_name("error_page")
             return
-        self.clamp.set_child(question_page)
+
+
 
     def get_new_trivia_questions(self, amount=10, category=None, difficulty=None, question_type=None, token=None):
         base_url = "https://opentdb.com/api.php"
@@ -360,12 +291,12 @@ class TriviaWindow(Adw.ApplicationWindow):
             if response_code == 1:
                 if self.amount == 5:
                     toast = Adw.Toast(title="The API returned no results")
-                    self.toast_overlay.add_toast(toast)
+                    # self.toast_overlay.add_toast(toast)
                 self.amount = 5
                 return
             if response_code == 2:
                 toast = Adw.Toast(title="There was en error retrieving more questions")
-                self.toast_overlay.add_toast(toast)
+                # self.toast_overlay.add_toast(toast)
                 return
             if response_code == 3:
                 self.token = self.get_open_trivia_token()
@@ -396,7 +327,6 @@ class TriviaWindow(Adw.ApplicationWindow):
             return 0
 
         json_data = response.json()
-
         category_names = []
 
         try:
@@ -407,67 +337,9 @@ class TriviaWindow(Adw.ApplicationWindow):
                 category_id = category.get("id")
                 if category_name:
                     category_names.append([category_name, category_id])
+                    self.categories_string_list.append(category_name)
 
         except json.JSONDecodeError as e:
             print("Error decoding JSON:", e)
 
         return category_names
-
-    def new_combo_row_from_strings(self, name, strings):
-        model_widget = Gio.ListStore(item_type=ListString)
-        sort_model_widget  = Gtk.SortListModel(model=model_widget)
-        filter_model_widget = Gtk.FilterListModel(model=sort_model_widget)
-        custom_filter_model = Gtk.CustomFilter.new() #self._do_filter_widget_view, filter_model_widget
-        filter_model_widget.set_filter(custom_filter_model)
-
-        for string in strings:
-            model_widget.append(ListString(string[0], string[1]))
-
-        factory_widget = Gtk.SignalListItemFactory()
-        factory_widget.connect("setup", self._on_factory_widget_setup)
-        factory_widget.connect("bind", self._on_factory_widget_bind)
-
-        ddwdg = Adw.ComboRow(title=name, model=filter_model_widget, factory=factory_widget, margin_top=6)
-        # ddwdg.set_enable_search(True)
-
-        # search_entry_widget = self._get_search_entry_widget(ddwdg)
-        # custom_filter_model.set_filter_func(self._do_filter_drop_down, filter_model_widget, search_entry_widget)
-        # search_entry_widget.connect('search-changed', self._on_search_drop_down_changed, custom_filter_model)
-
-        return ddwdg
-
-    def _get_search_entry_widget(self, dropdown):
-        popover = dropdown.get_last_child()
-        box = popover.get_child()
-        box2 = box.get_first_child()
-        search_entry = box2.get_first_child() # Gtk.SearchEntry
-        return search_entry
-
-    def _on_factory_widget_setup(self, factory, list_item):
-        box = Gtk.Box(spacing=6, orientation=Gtk.Orientation.HORIZONTAL)
-        label = Gtk.Label(ellipsize=3)
-        box.append(label)
-        list_item.set_child(box)
-
-    def _on_factory_widget_bind(self, factory, list_item):
-        box = list_item.get_child()
-        label = box.get_first_child()
-        widget = list_item.get_item()
-        label.set_text(widget.name)
-
-    def _on_selected_widget(self, dropdown, data):
-        widget = dropdown.get_selected_item()
-
-        name = widget.name
-        obj = eval(name)
-        a = set(dir(obj))
-        b = set(dir(Gtk.Widget))
-        c = a - b
-        for item in sorted(list(c)):
-            self.model_method.append(Method(name=item))
-
-    def _on_search_drop_down_changed(self, search_entry, filter_model):
-        filter_model.changed(Gtk.FilterChange.DIFFERENT)
-
-    def _do_filter_drop_down(self, item, filter_list_model, search_entry):
-        return search_entry.get_text().upper() in item.name.upper()
